00877998c24b108d695b6d9b13694e8adc89a0cc
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-02-12 22:24:37 +0330

    Initialized Card component

---
+++ src/Components/Card.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import CoverImage from "../Assets/Images/Cover.jpg";
+import { CSSTransition } from "react-transition-group";
+
+const Card = ({ id, url, code, isFlipped, RevealCard }) => {
+  useEffect(() => {
+    document.getElementById(id).style.transform = isFlipped
+      ? "rotateY(0deg)"
+      : "rotateY(180deg)";
+    document.getElementById(id).style.transition = isFlipped
+      ? "transform 0.7s ease-in-out"
+      : "none";
+    return () => {};
+  }, [isFlipped]);
+
+  return (
+    <Container>
+      <CardBody id={id} isFlipped={isFlipped} onClick={(e) => RevealCard(e)}>
+        <CardFront></CardFront>
+        <CardBack url={url}></CardBack>
+      </CardBody>
+    </Container>
+  );
+};
+
+export default Card;
+
+// Styles
+const Container = styled.div`
+  background-color: transparent;
+  width: min(20vw, 15em);
+  height: min(20vw, 15em);
+  perspective: 1000px;
+`;
+
+const CardBody = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+`;
+
+const SharedStyles = `
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border-radius: 8px;
+`;
+
+const CardFront = styled.div`
+  ${SharedStyles};
+  background-image: url(${CoverImage});
+  background-size: cover;
+  color: black;
+  transform: rotateY(180deg);
+`;
+
+const CardBack = styled.div`
+  ${SharedStyles};
+  background-image: url(${(props) => props.url});
+  background-size: cover;
+`;
